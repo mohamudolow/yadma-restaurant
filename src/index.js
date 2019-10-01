42f3897e89fe4d57d1b90fbe3a44c2de7a6bcc15
Mohamud Olow <khalif18@live.com>
2019-09-30 22:42:10 -0700

    add react to the website

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+class GeneralInquiries extends React.Component {
+    render() {
+        return <h1 style={{textAlign: "center"}}>General Inquiries Form</h1>;
+    }
+}
+
+ReactDOM.render(<GeneralInquiries />, document.getElementById("general-inquiries"));
